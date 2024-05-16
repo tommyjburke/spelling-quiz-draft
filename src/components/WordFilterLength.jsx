bfba2a41aab877305078350afe4187b4bbb2072f
Tommy J Burke <thatgingerdude@gmail.com>
2024-05-17 00:28:42 +0100

    refined word length filter

--- src/components/WordFilterLength.jsx
+++ src/components/WordFilterLength.jsx
@@ -1,5 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import RandomWordSelector from './RandomWordSelector'
+import {
+   Modal,
+   Checkbox,
+   Item,
+   Slider,
+   Button,
+   Col,
+   Row,
+   InputNumber,
+} from 'antd'
 
 function analyzeWordLengths(allWords) {
    if (allWords.length === 0) {
@@ -31,8 +41,8 @@ export default function WordFilterLength({ key, allWords }) {
    console.log('min', min)
    console.log('max', max)
 
-   const [minLength, setMinLength] = useState(min)
-   const [maxLength, setMaxLength] = useState(max)
+   const [minLength, setMinLength] = useState(parseInt(min))
+   const [maxLength, setMaxLength] = useState(parseInt(max))
    const [filteredWords, setFilteredWords] = useState(allWords)
 
    //    console.log('FILTERED: ', AllWords)
@@ -47,6 +57,18 @@ export default function WordFilterLength({ key, allWords }) {
       const upperValue = parseInt(e.target.value)
       setMaxLength(upperValue)
       filterWords(minLength, upperValue)
+   }
+
+   const handleWordLengthSlider = (sliderArray) => {
+      setMinLength(sliderArray[0])
+      setMaxLength(sliderArray[1])
+      filterWords(minLength, maxLength)
+   }
+
+   const handleSliderBoxLower = (value) => {
+      setMinLength(value)
+      // setMaxLength(sliderArray[1])
+      filterWords(minLength, maxLength)
    }
 
    const filterWords = () => {
@@ -70,18 +92,81 @@ export default function WordFilterLength({ key, allWords }) {
 
    return (
       <div>
-         <label>Minimum Word Length:</label>
-         <input
-            type='number'
-            value={minLength}
-            onChange={handleMinLengthChange}
-         />
-         <label>Maximum Word Length:</label>
-         <input
-            type='number'
-            value={maxLength}
-            onChange={handleMaxLengthChange}
-         />
+         {/* <div>
+            <label>Minimum Word Length:</label>
+            <input
+               type='number'
+               value={minLength}
+               onChange={handleMinLengthChange}
+            />
+            <label>Maximum Word Length:</label>
+            <input
+               type='number'
+               value={maxLength}
+               onChange={handleMaxLengthChange}
+            />
+         </div> */}
+
+         <div
+            style={{
+               display: 'flex',
+               // flexDirection: 'row',
+               justifyContent: 'center',
+               // alignItems: 'center',
+               // textAlign: 'center',
+               width: '100%',
+            }}
+         >
+            {/* <Row>
+               <Col span={3}> */}
+            <InputNumber
+               min={min}
+               max={max}
+               value={minLength}
+               onChange={(value) => setMinLength(value)}
+               style={{
+                  margin: '0 12px',
+                  width: '60px',
+               }}
+            />
+            {/* </Col>
+               <Col span={4}> */}
+            <Slider
+               style={{ width: '200px' }}
+               horizontal
+               range
+               step={1}
+               min={min}
+               max={max}
+               value={[minLength, maxLength]}
+               trackStyle={{
+                  backgroundColor: 'lightgreen',
+               }}
+               railStyle={{ backgroundColor: 'grey' }}
+               onChange={(value) => {
+                  // setMinLength(value[0])
+                  // setMaxLength(value[1])
+                  // // setAValue(value)
+                  // console.log('Slider: ', value)
+                  handleWordLengthSlider(value)
+               }}
+            />
+            {/* </Col>
+               <Col span={3}> */}
+            <InputNumber
+               style={{
+                  margin: '0 12px',
+                  width: '60px',
+               }}
+               min={min}
+               max={max}
+               value={maxLength}
+               onChange={(value) => setMaxLength(value)}
+            />
+            {/* </Col>
+            </Row> */}
+         </div>
+
          <h2>Filtered Words:</h2>
 
          <p>
